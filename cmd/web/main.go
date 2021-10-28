e3f20bbeffa94f82a37394cc733c691fe0e47c73
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-10-28 15:49:31 +0900

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"net/http"
+
+	"github.com/y-kouhei9/bookings/pkg/config"
+	"github.com/y-kouhei9/bookings/pkg/handlers"
+	"github.com/y-kouhei9/bookings/render"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function.
+func main() {
+	// chage this to true when in production.
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Connot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server {
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
