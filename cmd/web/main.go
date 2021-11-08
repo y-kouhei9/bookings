04f3fe95d96549a4f9183659337c8e9839ba4cf8
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-08 16:44:34 +0900

    Adding connecting database function

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"github.com/y-kouhei9/bookings-app/internal/driver"
 	"encoding/gob"
 	"fmt"
-	"github.com/alexedwards/scs/v2"
-	"github.com/tsawler/bookings-app/internal/config"
-	"github.com/tsawler/bookings-app/internal/handlers"
-	"github.com/tsawler/bookings-app/internal/helpers"
-	"github.com/tsawler/bookings-app/internal/models"
-	"github.com/tsawler/bookings-app/internal/render"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/y-kouhei9/bookings-app/internal/config"
+	"github.com/y-kouhei9/bookings-app/internal/handlers"
+	"github.com/y-kouhei9/bookings-app/internal/helpers"
+	"github.com/y-kouhei9/bookings-app/internal/models"
+	"github.com/y-kouhei9/bookings-app/internal/render"
 )
 
 const portNumber = ":8080"
@@ -24,10 +26,11 @@ var errorLog *log.Logger
 
 // main is the main function
 func main() {
-	err := run()
+	db, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.SQL.Close()
 
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 
@@ -42,7 +45,7 @@ func main() {
 	}
 }
 
-func run() error {
+func run() (*driver.DB, error) {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
@@ -64,19 +67,28 @@ func run() error {
 
 	app.Session = session
 
+	// connect to database
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings user=kouheiyamamoto password=")
+	if err != nil {
+		log.Fatal("Cannot connect to database! Dying...")
+	}
+	defer db.SQL.Close()
+	log.Println("Connected to database!")
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
-		return err
+		return nil, err
 	}
 
 	app.TemplateCache = tc
 	app.UseCache = false
 
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 	helpers.NewHelpers(&app)
 
-	return nil
+	return db, nil
 }
