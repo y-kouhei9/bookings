d0521961b5fa4af2fbff420bf10ad0d5a1f3c65c
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-08 16:45:31 +0900

    Creating a repository directory

---
+++ internal/repository/dbrepo/dbrepo.go
@@ -0,0 +1,21 @@
+package dbrepo
+
+import (
+	"github.com/y-kouhei9/bookings-app/internal/repository"
+	"database/sql"
+	"github.com/y-kouhei9/bookings-app/internal/config"
+	
+)
+
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+// NewPostgresRepo 
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB: conn,
+	}
+}
