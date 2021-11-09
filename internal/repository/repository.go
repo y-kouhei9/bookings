562a998e802dfce5e703ae1691c6572d6e70b611
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-09 15:22:43 +0900

    Define DatabaseRepo interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,11 @@
 package repository
 
+import (
+	"github.com/y-kouhei9/bookings-app/internal/models"
+)
+
 type DatabaseRepo interface {
 	AllUsers() bool
-}
 
+	InsertReservation(res models.Reservation) error
+}
