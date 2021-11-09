f752ce29c3027693bbce9cd5e0d6eb54ee0f8245
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-09 15:19:11 +0900

    Updated models of struct

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -1,6 +1,6 @@
 package models
 
-import "github.com/tsawler/bookings-app/internal/forms"
+import "github.com/y-kouhei9/bookings-app/internal/forms"
 
 // TemplateData holds data sent from handlers to templates
 type TemplateData struct {
