76f57c3ce76be4b841d359b1c44573470a727953
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-10 18:38:44 +0900

    Appending func into DatabaseRepo interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,11 +1,15 @@
 package repository
 
 import (
+	"time"
 	"github.com/y-kouhei9/bookings-app/internal/models"
 )
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
