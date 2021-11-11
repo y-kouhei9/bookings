f22d49203fddb88fccd80fcca0535068091da3a0
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-11 13:28:18 +0900

    Create seeding tables

---
+++ migrations/20211111042426_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s quarter','2020-11-17 00:00:00.000','2020-11-19 00:00:00.000'),
+	 ('Major''t suite','2020-11-17 00:00:00.000','2020-11-19 00:00:00.000');
