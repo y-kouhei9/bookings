f94d1805c2d8aebc1e87db0613983565e00950f9
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-12 10:44:19 +0900

    Create listenForMail func

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -47,6 +47,12 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(mailChan)
+
+	listenForMail()
+
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -61,6 +67,16 @@ func TestMain(m *testing.M) {
 	render.NewRenderer(&app)
 
 	os.Exit(m.Run())
+}
+
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <- app.MailChan
+			
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
