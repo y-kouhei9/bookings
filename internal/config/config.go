5b1f432918edaa4f1da828ffdce9ec2b76553661
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-12 10:46:16 +0900

    Append MailChan property on AppConfig struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/y-kouhei9/bookings-app/internal/models"
 	"github.com/alexedwards/scs/v2"
 	"html/template"
 	"log"
@@ -14,4 +15,5 @@ type AppConfig struct {
 	ErrorLog      *log.Logger
 	InProduction  bool
 	Session       *scs.SessionManager
+	MailChan	  chan models.MailData
 }
