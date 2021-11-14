693ad936954621823788b0b143739cf70ee2ca1f
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-11-14 15:36:03 +0900

    Add comment

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -24,7 +24,7 @@ func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
 }
 
-
+// Auth handles auth permission
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !helpers.IsAuthenticated(r) {
